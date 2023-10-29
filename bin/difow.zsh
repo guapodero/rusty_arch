9acfece34bbfa1eabac07996844fcc6ebbd6ff8e
Daniel James Baumann <568034+guapodero@users.noreply.github.com>
2023-10-29 16:55:47 -0500

    improvements to difow.zsh backup script

--- bin/difow.zsh
+++ bin/difow.zsh
@@ -1,44 +1,80 @@
 #!/bin/zsh
 
+POSITIONAL_ARGS=()
+IGNORE_PATTERNS=("*/target/*" "*/.DS_Store")
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -i|--ignore)
+            IGNORE_PATTERNS+=("$2")
+            shift # past argument
+            shift # past value
+            ;;
+        -*|--*)
+            echo "unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 if [[ $# -ne 2 ]]; then
-    >&2 echo "usage: $0 src dst"
+    >&2 echo "usage: $0 [(-i | --ignore) glob] SRC DST"
     >&2 echo ""
-    >&2 echo "backup files from src to dst"
+    >&2 echo "backup files from SRC to DST"
     >&2 echo "show diff and wait for approval"
-    >&2 echo "overwrites the destination directory completely"
     exit 1
 fi
 
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-RESET="$(tput sgr0)"
+rsync --version 2> /dev/null | head -1 | grep rsync
+if [[ $? -ne 0 ]]; then
+    >&2 echo "rsync is required" # rsync is ubiquitous
+    exit 1
+fi
 
-src=$1; dst=$2
+SRC=$1
+DST=$2
+if ! [[ -d $SRC && -r $SRC ]]; then
+    >&2 echo "${SRC} is not a readable directory"
+    exit 1
+fi
 
-[[ -d $src && -r $src ]] || (>&2 echo "${src} isn't a readable directory" && exit 1)
+echo "$(tput setaf 1)SRC: ${SRC}\n$(tput setaf 2)DST: ${DST}\n$(tput sgr0)"
 
-info="${RED} src: ${src} \n ${GREEN} dst: ${dst} \n ${RESET}"
+FIND_FILTER=""
+for pattern in "${IGNORE_PATTERNS[@]}"; do
+    FIND_FILTER+=("-not -path $pattern")
+done
 
 sha_tree() {
-    find $1 -not -path "*/.git/*" -not -path "*/target/*" -not -path "*/.DS_Store" \
-        | xargs -L 1 bash -c 'if ! [ -d $0 ] ; then sha1sum "$0"; fi' \
-        | sed "s#$1##" \
-        | sort -k 2
+    find $1 -not -path "*/.git/*" ${(z)FIND_FILTER} \
+    | xargs -L 1 bash -c 'if ! [ -d $0 ] ; then sha1sum "$0"; fi' \
+    | sed "s#$1##" \
+    | sort -k 2
 }
 
-diff --color <(sha_tree $dst) <(sha_tree $src)
+diff --color <(sha_tree $DST) <(sha_tree $SRC)
 
-if [ $? -eq 0 ]; then
-    echo "${info}no changes detected"
+if [[ $? -eq 0 ]]; then
+    echo "no differences detected"
     exit 0
 fi
 
-echo "${info}waiting for approval or ^C"
-read wait
+echo "continue with sync? (includes .git)"
+read confirm
+[[ "$confirm" == "" || "$confirm" == [Yy]* ]] || exit 1
 
-# remove unwanted files before cp
-find $src -type d -name target -prune -exec rm -r {} +
-find $src -type f -name .DS_Store -delete
-# overwrite
-rm -rf $dst
-cp -r $src $dst
+RSYNC_FILTER=""
+for pattern in "${IGNORE_PATTERNS[@]}"; do
+    # convert patterns like */target/* to target for rsync
+    pattern=${pattern##\*/} # remove */ from start
+    pattern=${pattern%%/\*} # remove /* from end
+    RSYNC_FILTER+=("--exclude $pattern")
+done
+
+rsync -r --delete --out-format="%i %n" ${(z)RSYNC_FILTER} $SRC/ $DST | grep -v ".git/*"
--- dot_config/zsh/my_profile.zsh
+++ dot_config/zsh/my_profile.zsh
@@ -2,8 +2,12 @@ alias hx='helix'
 alias shx='sudo /usr/bin/helix -c ~/.config/helix/config.toml'
 
 mkdir -p ~/scratch
-alias bak_scratch='() { ${HOST_WORKDIR}/bin/difow.zsh ~/scratch/$1 ${HOST_WORKDIR}/data/scratch_bak/$1 }'
-alias bak_hctarcs='() { ${HOST_WORKDIR}/bin/difow.zsh ${HOST_WORKDIR}/data/scratch_bak/$1 ~/scratch/$1 }'
+alias bak_scratch='() { $HOST_WORKDIR/bin/difow.zsh ~/scratch/$1 $HOST_WORKDIR/data/scratch_bak/$1 }'
+alias bak_hctarcs='() { $HOST_WORKDIR/bin/difow.zsh $HOST_WORKDIR/data/scratch_bak/$1 ~/scratch/$1 }'
+
+alias bak_config='$HOST_WORKDIR/bin/difow.zsh \
+    -i "*/buildkit/*" -i "*/cni/*" -i "*/containerd*" -i "*/systemd/*" \
+    $XDG_CONFIG_HOME $HOST_WORKDIR/dot_config'
 
 gh_clone () {
     mkdir -p ~/github
@@ -11,3 +15,5 @@ gh_clone () {
     git clone git@github.com:$1.git
     cd ${1##*/}
 }
+
+cd ~/scratch
